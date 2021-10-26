28f0f28a39bd3bf2daa6385299b1df8a31a01603
Naol Legesse <nlege001@plattsburgh.edu>
2021-10-25 20:16:09 -0400

    sample profile picture and "any" email attached on recycler view for view assessments

--- app/src/main/java/com/example/accounttcreation/DisplayUserInput.kt
+++ app/src/main/java/com/example/accounttcreation/DisplayUserInput.kt
@@ -5,7 +5,13 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.*
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.android.synthetic.main.activity_profile.*
+import kotlinx.android.synthetic.main.activity_profile.image_view
+import kotlinx.android.synthetic.main.list_item_display.*
 
 class DisplayUserInput : AppCompatActivity() {
 
@@ -14,9 +20,14 @@ class DisplayUserInput : AppCompatActivity() {
     private lateinit var myAdapter : DisplayAdapterClass
     private lateinit var db : FirebaseFirestore
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display_user_input)
+
+
+
+
 
         recyclerView = findViewById(R.id.recyclerView_display)
         recyclerView.layoutManager = LinearLayoutManager(this)
