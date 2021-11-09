17f9afa6a44c38bd7d1b3841a5633439a369400c
Naol Legesse <nlege001@plattsburgh.edu>
2021-11-08 21:23:30 -0500

    Individual items can be clicked after using the search button in "see assessments"

--- app/src/main/java/com/example/accounttcreation/DisplayUserInput.kt
+++ app/src/main/java/com/example/accounttcreation/DisplayUserInput.kt
@@ -49,19 +49,21 @@ class DisplayUserInput : AppCompatActivity() {
         recyclerView.adapter = myAdapter
 
         EvenChangeListener()
+        
+
 
         myAdapter.setOnItemClickListener(object : DisplayAdapterClass.onItemClickListener{
             override fun onItemClick(position: Int) {
                 //Toast.makeText(this@DisplayUserInput, "you clicked on item $position", Toast.LENGTH_SHORT).show()
                 val intent = Intent(this@DisplayUserInput, DisplayIndividualUserInput::class.java )
-                intent.putExtra("profName", userArrayList[position].proffessor)
-                intent.putExtra("courseNumber", userArrayList[position].courseNumber)
-                intent.putExtra("semester", userArrayList[position].semester)
-                intent.putExtra("difficulty", userArrayList[position].difficlty)
-                intent.putExtra("courseRating", userArrayList[position].courseRating)
-                intent.putExtra("grade", userArrayList[position].grade)
-                intent.putExtra("comments", userArrayList[position].comments)
-                intent.putExtra("email", userArrayList[position].email)
+                intent.putExtra("profName", tempArrayList[position].proffessor)
+                intent.putExtra("courseNumber",tempArrayList[position].courseNumber)
+                intent.putExtra("semester", tempArrayList[position].semester)
+                intent.putExtra("difficulty", tempArrayList[position].difficlty)
+                intent.putExtra("courseRating", tempArrayList[position].courseRating)
+                intent.putExtra("grade", tempArrayList[position].grade)
+                intent.putExtra("comments", tempArrayList[position].comments)
+                intent.putExtra("email", tempArrayList[position].email)
                 startActivity(intent)
 
             }
