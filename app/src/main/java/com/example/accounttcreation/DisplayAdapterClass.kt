a86433a41d29197f1cad91237d505d3561fd80b4
Naol Legesse <nlege001@plattsburgh.edu>
2021-12-06 10:25:49 -0500

    Card view for recycler view edited

--- app/src/main/java/com/example/accounttcreation/DisplayAdapterClass.kt
+++ app/src/main/java/com/example/accounttcreation/DisplayAdapterClass.kt
@@ -4,12 +4,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.RatingBar
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.nav_profile_header.*
+import org.w3c.dom.Text
 
 class DisplayAdapterClass(private val userList : ArrayList<DisplayDataClass>) : RecyclerView.Adapter<DisplayAdapterClass.MyviewHolder>() {
     private lateinit var mListener : onItemClickListener
@@ -24,7 +26,7 @@ class DisplayAdapterClass(private val userList : ArrayList<DisplayDataClass>) : 
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DisplayAdapterClass.MyviewHolder {
-        val itemview = LayoutInflater.from(parent.context).inflate(R.layout.list_item_display, parent, false)
+        val itemview = LayoutInflater.from(parent.context).inflate(R.layout.card_individual_display, parent, false)
         return MyviewHolder(itemview, mListener)
     }
 
@@ -32,13 +34,13 @@ class DisplayAdapterClass(private val userList : ArrayList<DisplayDataClass>) : 
 
         val input : DisplayDataClass = userList[position]
         holder.professor.text = input.proffessor
-        holder.courseNumber.text = input.courseNumber
         holder.semester.text = input.semester
-        holder.difficulty.text = input.difficlty
-        holder.courseRating.text = input.courseRating
-        holder.grade.text = input.grade
-        holder.comments.text = input.comments
         holder.email.text = input.email
+        holder.likeCount.text = input.likeCount.toString()
+        holder.dislikeCount.text = input.dislikeCount.toString()
+        holder.ratingBar.rating = input.courseRating?.toFloat()!!
+        holder.displayRatingBar.text = input.courseRating
+        holder.courseName.text = input.courseName
 
         val downloadURLPATH = input[8]
         Glide.with(holder.itemView.context).load(downloadURLPATH).into(holder.profilePic)
@@ -54,15 +56,17 @@ class DisplayAdapterClass(private val userList : ArrayList<DisplayDataClass>) : 
 
     public class MyviewHolder(itemview:View, listener: onItemClickListener): RecyclerView.ViewHolder(itemview){
 
-        val professor : TextView = itemview.findViewById(R.id.profName_card)
-        val courseNumber : TextView = itemview.findViewById(R.id.crn_card)
-        val semester : TextView = itemview.findViewById(R.id.semester_card)
-        val difficulty : TextView = itemview.findViewById(R.id.difficulty_card)
-        val courseRating : TextView = itemview.findViewById(R.id.cRating_card)
-        val grade : TextView = itemview.findViewById(R.id.garde_card)
-        val comments : TextView = itemview.findViewById(R.id.comments_card)
-        val email : TextView = itemview.findViewById(R.id.email_profile_text)
-        val profilePic : ImageView = itemview.findViewById(R.id.image_view_profile)
+        val professor : TextView = itemview.findViewById(R.id.individual_card_profName_card)
+        val courseName : TextView = itemview.findViewById(R.id.individual_card_crn_card_display)
+        val semester : TextView = itemview.findViewById(R.id.individual_semester_card_display)
+        val email : TextView = itemview.findViewById(R.id.individual_card_email_profile_text)
+        val profilePic : ImageView = itemview.findViewById(R.id.individual_card_image_view_profile)
+        val likeCount : TextView = itemview.findViewById(R.id.individual_card_like_count)
+        val dislikeCount : TextView = itemview.findViewById(R.id.individual_card_dislike_count)
+        val ratingBar : RatingBar = itemview.findViewById(R.id.individual_course_rating_bar)
+        val displayRatingBar : TextView = itemview.findViewById(R.id.individual_ratingbar_display)
+
+
 
 
         init {
